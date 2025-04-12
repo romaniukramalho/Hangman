90c9e28f70bc37c366c7c04aafe88519076571b5
romaniukramalho <arturramalhocs@gmail.com>
2025-04-11 23:54:28 -0300

    add: first commit: git remote add origin git@github.com:romaniukramalho/Hangman.git git push -u origin main lear clear

---
+++ forca.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <time.h>
+
+void sortWord(char* wordbank)
+
+int main()
+{
+    char* wordbank[] =  //banco de 34 palavras disponiveis no jogo da forca//
+    {
+        "cachorro", "gavião", "tubarão", "chimpanze", "urso", "papagaio",
+        "cobra  marinha", "espaconave", "leao", "raposa", "elefante",
+        "rato grande", "rato cumprido", "rato anao", "rato aquatico", "rato voador",
+        "alce", "guaxinim", "dinossauro", "galinha", "camelo", "camarao", "polvo",
+        "mistura de pato com foca", "caranguejo", "escorpião", "pelicano", "cisne",
+        "filhote de elefante", "bicho preguiça", "tartaruga", "pinguim", "crocodilo", "periquito"
+    };
+
+}
+
+void sortWord(char* wordbank)
+{
+
+}
