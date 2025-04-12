bb8094911dfc11ee7dc9c0d3f19f7508599577cc
romaniukramalho <arturramalhocs@gmail.com>
2025-04-12 02:06:28 -0300

    add:gitignote, funcao menu, funcao sort e comeco funcao game

--- forca.c
+++ forca.c
@@ -1,25 +1,72 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdlib.h>
 #include <time.h>
 
-void sortWord(char* wordbank)
-
+int sortWord(char *wordbank[]);
+void Game(char *wordbank[],int animalrand, char *animal);
+void menu(char *animal);
 int main()
 {
-    char* wordbank[] =  //banco de 34 palavras disponiveis no jogo da forca//
+    char *wordbank[] =  //banco de 34 palavras disponiveis no jogo da forca//
     {
-        "cachorro", "gavião", "tubarão", "chimpanze", "urso", "papagaio",
+        " ", "cachorro", "gaviao", "tubarao", "chimpanze", "urso", "papagaio",
         "cobra  marinha", "espaconave", "leao", "raposa", "elefante",
         "rato grande", "rato cumprido", "rato anao", "rato aquatico", "rato voador",
         "alce", "guaxinim", "dinossauro", "galinha", "camelo", "camarao", "polvo",
-        "mistura de pato com foca", "caranguejo", "escorpião", "pelicano", "cisne",
-        "filhote de elefante", "bicho preguiça", "tartaruga", "pinguim", "crocodilo", "periquito"
+        "mistura de pato com foca", "caranguejo", "escorpiao", "pelicano", "cisne",
+        "filhote de elefante", "bicho preguica", "tartaruga", "pinguim", "crocodilo", "periquito"
     };
-
+    int animalrand = sortWord(wordbank);
+    char animal[30];
+    strcpy(animal, wordbank[animalrand]);
+    menu(animal);
+    //printf("o animal sorteado foi %s \n", animal);
+    printGame(wordbank, animalrand, animal);
 }
-
-void sortWord(char* wordbank)
+int sortWord(char *wordbank[])
 {
+    srand(time(NULL));
+    int sorted;
 
+    sorted = (rand() % 34) + 1;
+    //printf("o animal sorteado foi %s ", wordbank[sorted]);
+
+    return sorted;
+}
+void Game(char *wordbank[],int animalrand, char *animal)
+{
+    int letra;
+    prinf("Digite uma letra: \n")
+    scanf("%c", &letra);
+    letra = tolower(letra);
+    int i;
+    char comp = 'a'
+    for(i = 0; i < 25; i++)
+    {
+        if(letra == (comp + i))
+        {
+            //termino depois
+        }
+    }
+}
+void menu(char *animal)
+{
+    system("clear");
+    printf("-Bem vindo ao jogo da forca, o objetivo eh tentar adivinhar o nome dos animais antes que acabe as tentativas-\n");
+    printf("O tema da partida sera animais, alguns de procedencia duvidosa, boa sorte\n\nAperte Enter para inicar a partida\n");
+    
+    getchar();
+
+    int i;
+    for(i = 0; i < strlen(animal); i++)
+    {
+        if(animal[i] == ' ')
+        {
+            printf("  ");
+        }
+        printf("_ ");
+    }
+    printf("\n");
 }
